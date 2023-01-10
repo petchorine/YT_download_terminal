0145e1d1d66ce6895115b9919dda203b96e3568d
Christophe <christophe.couilleault@proton.com>
2023-01-10 10:32:04 +0100

    ajout de la documentation

--- main.py
+++ main.py
@@ -25,6 +25,20 @@ def introduction():
     """)
 
 def get_url_and_verify_integrity():
+    """
+    - demande l'url du clip à télécharger à l'utilisateur
+    - vérifie que l'url commence bien avec "https://www.youtube.com"
+        - si ok => retourne user_url
+        - sinon redemande une url correcte
+
+    Paramètres:
+    ==========
+
+
+    Retourne:
+    =========
+        user_url (str) : url du clip à télécharger
+    """
     while True:
         print("\n")
         print("Copier-coller l'url de la video ou la playlist que vous souhaitez télécharger.")
@@ -42,6 +56,17 @@ def get_url_and_verify_integrity():
 
 
 def is_not_playlist(url):
+    """
+        vérifie si l'élément associé à l'url est une playlist ou un clip unique
+
+    Paramètres:
+    ==========
+        url (str) : url du clip à télécharger
+
+    Retourne:
+    =========
+        bool
+    """
     if "playlist" not in url:
         return True
     else:
@@ -49,6 +74,19 @@ def is_not_playlist(url):
 
 
 def show_playlist(url):
+    """
+    - que l'url corresponde à une playlit ou non...
+      ...constitue une liste de toutes les videos (objets PyTube) associés à l'url
+    - affiche le titre et la durée pour chaque vidéo
+
+    Paramètres:
+    ==========
+        url (str) : url du clip à télécharger
+
+    Retourne:
+    =========
+        liste d'objets Pytube
+    """
     playlist_youtube_video = []
     if is_not_playlist(url) == True:
         youtube_video = YouTube(url)
@@ -70,7 +108,20 @@ def show_playlist(url):
     return playlist_youtube_video
   
 
-def choice_audio_or_video():    
+def choice_audio_or_video():
+    """
+        demande à l'utilisateur le type de média à télécharger : 
+            - video + audio
+            - seulement audio
+
+    Paramètres:
+    ==========
+
+
+    Retourne:
+    =========
+        user_choice (str) : soit "1", soit "2"
+    """    
     print("\n")
     print("Tu veux télécharger le(s) clip(s) audio/vidéo ou seulement l'audio ?")
     print("   1 - le(s) clip(s) audio/vidéo")
@@ -90,6 +141,26 @@ def choice_audio_or_video():
 
 
 def resolution_choice(choice_media_type, playlist):
+    """
+    Si choix utilisateur == "1" (video + audio)
+        demande à l'utilisateur la qualité à télécharger :
+            - optimale => meilleure qualité si progressive = False
+            - moyenne => meilleure qualité si progressive = True
+            - minimale => plus basse qualité si progressive = True
+        appelle la fonction permettant d'isoler les streams dans la qualité choisie
+    Si choix utilisateur == "2" (seulement audio)
+        - appelle la fonction qui pemet à l'utilisateur de choisir le dossier de téléchargement
+        - appelle la fonction pour télécharger l'audio dans la meilleure qualité disponible
+
+    Paramètres:
+    ==========
+        choice_media_type (str) : choix de l'utilisateur sur le type de média ("1" ou "2")
+        playlist (list) : liste des objets PyTube associés à l'url
+
+    Retourne:
+    =========
+
+    """ 
     if choice_media_type == "1":
         quality = ("qualité optimale", "qualité moyenne", "qualité minimale")
         print("\n")
@@ -112,6 +183,18 @@ def resolution_choice(choice_media_type, playlist):
         download_audio(playlist, user_directory_choice)
 
 def directory_choice():
+    """
+    permet de récupérer l'adresse du dossier de téléchargement choisi par l'utilisateur
+    si l'utilisateur ne choisit pas de dossier (clic sur fermeture de la fenêtre ou Annuler) 
+        - dossier par défaut est le dossier où se trouve le script
+
+    Paramètres:
+    ==========
+        
+    Retourne:
+    =========
+    user_directory_choice (str) : adresse du dossier de téléchargement choisi par l'utilisateur
+    """ 
     # on initialise tkinter en appelant la fonction Tk()
     racine_tk = tkinter.Tk()
     # permet de masquer la petite fenêtre supplémentaire (je ne sais pas à quoi elle sert ???)
@@ -131,7 +214,37 @@ def directory_choice():
 
     return user_directory_choice
 
-def get_resolutions_list(playlist, user_quality_choice):    
+def get_resolutions_list(playlist, user_quality_choice):
+    """
+    En fonction du choix de qualité par l'utilisateur :
+        - pour la qualité optimale ("1") :
+            - crée une liste triée (décroissant) de tous les clips :
+                - suivant resolution pour la video + progressive = False
+                - suivant abr pour l'audio 
+            - crée une liste des vidéos avec seulement meilleure qualité et progressive = False)
+            - crée une liste des audio avec seulement meilleure qualité disponible
+            - appelle la fonction de téléchargement avec en paramètres, les listes constituées
+        - pour la qualité moyenne ("2") et minimale ("3") :
+            - crée une liste triée (décroissant) de tous les clips
+                - suivant resolution pour la video + progressive = True
+            - appelle la fonction de téléchargement avec en paramètre, la liste constituée
+
+    Paramètres:
+    ==========
+    playlist (list) : liste des objets PyTube associés à l'url
+    user_quality_choice (int) : choix de la qualité (1, 2 ou 3) par l'utilisateur
+        
+    Retourne:
+    =========
+    - pour la qualité optimale ("1") :
+        - best_video_resolutions_list (list) : liste des streams video dans la meilleure qualité
+        - best_audio_resolutions_list (list) : liste des streams audio dans la meilleure qualité
+        - user_directory_choice (str) : adresse du dossier de téléchargement choisie par l'utilisateur
+    - pour la qualité moyenne ("2") et minimale ("3") :
+        - resolutions_list (list) : liste des streams dans la qualité demandée
+        - user_directory_choice (str) : adresse du dossier de téléchargement choisie par l'utilisateur
+    
+    """ 
     user_directory_choice = directory_choice()
     if user_quality_choice == 1:
         video_stream_list = []
@@ -170,6 +283,28 @@ def get_resolutions_list(playlist, user_quality_choice):
 
 
 def download_video(resolutions_list, user_directory_choice, list_audio=None):
+    """
+        En fonction du choix de qualité par l'utilisateur :
+        - pour la qualité optimale ("1") :
+            - crée 2 dossiers temporaires de téléchargement (audio et video) 
+            - télécharge les audios et les videos
+            - combine les fichiers dans la meilleure qualité disponible dans le dossier choisi
+            - efface les dossiers temporaires et leur contenu
+        - pour la qualité moyenne ("2") et minimale ("3") :
+            - télécharge les audios et les videos dans le dossier choisi
+   
+    
+    Paramètres:
+    ==========
+    - resolutions_list (list) : liste des streams video dans la resolution demandée
+    - user_directory_choice (str) : adresse du dossier de téléchargement choisie par l'utilisateur
+    - list_audio=None (list) : liste des streams audio dans la resolution demandée (optionnel si progressive = True)
+        
+    Retourne:
+    =========
+    
+    """
+    
     # TELECHARGEMENT AUDIO
     audio_temp_directory = fr"{user_directory_choice}\audio_ytdown"
     if list_audio is not None:
@@ -209,6 +344,19 @@ def download_video(resolutions_list, user_directory_choice, list_audio=None):
 
 
 def download_audio(playlist_audio_to_download, user_directory_choice):
+    """
+    crée une liste avec les audios triés en fonction de la qualité (abr) descendante 
+    télécharge les meilleures audios dans le dossier spécifié par l'utilisateur
+    
+    Paramètres:
+    ==========
+    playlist_audio_to_download (list) :
+    user_directory_choice (str) :
+        
+    Retourne:
+    =========
+    
+    """
     all_audio_streams = []
     print("\n")
     for clip in playlist_audio_to_download:
@@ -224,6 +372,18 @@ def download_audio(playlist_audio_to_download, user_directory_choice):
 
 
 def ending():
+    """
+    permet de relancer un téléchargement à la suite de celui qui vient de se terminer (choix 1)
+    ou
+    de quitter le programme (choix 2)
+    
+    Paramètres:
+    ==========
+        
+    Retourne:
+    =========
+    
+    """
     print("\n")
     print(f"Tous les clips ont bien été téléchargés...")
     print("\n")
@@ -242,6 +402,16 @@ def ending():
 
 
 def main():
+    """
+    fonction principale permettant de lancer le script
+    
+    Paramètres:
+    ==========
+        
+    Retourne:
+    =========
+    
+    """
     introduction()
     user_url_input = get_url_and_verify_integrity()
     is_not_playlist(user_url_input)
